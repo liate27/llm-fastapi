eded0fcec0f8b7975210da6c99ebd35233753ff5
Atul Karve <Atul.Karve@gmail.com>
2024-02-10 18:02:04 +0000

    Created a chatbot

---
+++ frontend/src/environment.js
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    // fastAPIUrl: "https://8000-monikakusumanc-reactapp-zn729xfad4j.ws-us108.gitpod.io/get_gemini_completion"
+    fastAPIUrl: "https://8000-liate27-llmfastapi-fuoxcoiafob.ws-us108.gitpod.io/get_gemini_completion"
+  };
